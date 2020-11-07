34e430654450086c74fd2c1b9096426e8a1cf7c8
nadun <nra@tiqri.com>
2020-11-07 12:30:15 +0530

    add missing files

---
+++ ElementLogic.AMS.UI.Tests/Pages/AdminModule/Automation/AutostoreEquipmentList/LiveFeedStatus/SynchronizeBinContentsPopup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SeleniumEssential;
+
+namespace ElementLogic.AMS.UI.Tests.Pages.AdminModule.Automation.AutostoreEquipmentList.LiveFeedStatus
+{
+    public class SynchronizeTaskGroupsPopup
+    {
+        private const string Popup = "#swal2-title";
+        
+        private const string SynchronizeButton = ".swal2-confirm";
+
+        public static SynchronizeTaskGroupsPopup Instance => Singleton.Value;
+
+        public bool IsPopupDisplayed()
+        {
+            return PageObjectHelper.Instance.IsDisplayed(Popup, true);
+        }
+
+        public bool ClickSynchronizeButton()
+        {
+            return PageObjectHelper.Instance.Click(SynchronizeButton);
+        }
+
+        private SynchronizeTaskGroupsPopup() { }
+
+        private static readonly Lazy<SynchronizeTaskGroupsPopup> Singleton =
+            new Lazy<SynchronizeTaskGroupsPopup>(() => new SynchronizeTaskGroupsPopup());
+    }
+}
---
+++ ElementLogic.AMS.UI.Tests/Pages/AdminModule/Automation/AutostoreEquipmentList/LiveFeedStatus/SynchronizeTaskGroupsPopup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SeleniumEssential;
+
+namespace ElementLogic.AMS.UI.Tests.Pages.AdminModule.Automation.AutostoreEquipmentList.LiveFeedStatus
+{
+    public class SynchronizeBinContentsPopup
+    {
+        private const string PopupTitle = "#swal2-title";
+        
+        private const string SynchronizeButton = ".swal2-confirm";
+
+        public static SynchronizeBinContentsPopup Instance => Singleton.Value;
+
+        public bool IsPopupDisplayed()
+        {
+            return PageObjectHelper.Instance.IsDisplayed(PopupTitle, true);
+        }
+
+        public bool ClickSynchronizeButton()
+        {
+            return PageObjectHelper.Instance.Click(SynchronizeButton);
+        }
+
+        private SynchronizeBinContentsPopup() { }
+
+        private static readonly Lazy<SynchronizeBinContentsPopup> Singleton =
+            new Lazy<SynchronizeBinContentsPopup>(() => new SynchronizeBinContentsPopup());
+    }
+}
